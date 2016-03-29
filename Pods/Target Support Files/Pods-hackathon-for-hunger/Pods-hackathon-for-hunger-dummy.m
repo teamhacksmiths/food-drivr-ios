5db1810b15a821dd5236fdc2d853354d699e09b9
Ian Gristock <iangristock@gmail.com>
2016-03-29 18:50:09 +0100

    chore: adding basic pod dependancies

---
+++ Pods/Target Support Files/Pods-hackathon-for-hunger/Pods-hackathon-for-hunger-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_hackathon_for_hunger : NSObject
+@end
+@implementation PodsDummy_Pods_hackathon_for_hunger
+@end
---
+++ Pods/Target Support Files/Pods-hackathon-for-hunger/Pods-hackathon-for-hunger-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_hackathon_for_hungerVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_hackathon_for_hungerVersionString[];
+
