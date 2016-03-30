d7100bdf725867fca72d30659169ce42fde955e1
Ian Gristock <iangristock@gmail.com>
2016-03-30 19:52:02 +0100

    feat: adding base router into app, along with demo endpoint

---
+++ hackathon-for-hunger/Donation.swift
@@ -0,0 +1,16 @@
+//
+//  Donation.swift
+//  hackathon-for-hunger
+//
+//  Created by Ian Gristock on 3/30/16.
+//  Copyright © 2016 Hacksmiths. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class Donation: Object {
+    
+    dynamic var id: String!
+    
+}
