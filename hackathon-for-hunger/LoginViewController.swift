597acc685f837f3f70ac7dcf2b9a4519d09e66b6
Anas Belkhadir <belkhadir.anas@gmail.com>
2016-04-01 15:12:53 +0100

    chore: change the name of the class

---
+++ hackathon-for-hunger/LoginViewController.swift
@@ -0,0 +1,26 @@
+//
+//  LoginViewController.swift
+//  hackathon-for-hunger
+//
+//  Created by Ian Gristock on 3/28/16.
+//  Copyright © 2016 Hacksmiths. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+
+}
+
