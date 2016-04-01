a38be93d859b979931ad6e142a211c628c47a218
Anas Belkhadir <belkhadir.anas@gmail.com>
2016-04-01 17:35:23 +0100

    feat: descipe the layout for the collection view

---
+++ hackathon-for-hunger/DashboardLayout.swift
@@ -0,0 +1,70 @@
+//
+//  DashboardLayout.swift
+//  hackathon-for-hunger
+//
+//  Created by Anas Belkhadir on 01/04/2016.
+//  Copyright © 2016 Hacksmiths. All rights reserved.
+//
+
+import UIKit
+
+protocol DashboardLayoutDelegate {
+    
+    func collectionView(collectionView: UICollectionView, heightForItemAtIndexPath indexPath: NSIndexPath) -> CGFloat
+    
+}
+
+class DashboardLayout: UICollectionViewLayout {
+    
+    var delegate: DashboardLayoutDelegate!
+    var numberOfColumns = 1
+    
+    private var cache = [UICollectionViewLayoutAttributes]()
+    private var contentHeight: CGFloat = 0
+    private var width: CGFloat {
+        get {
+            return CGRectGetWidth(collectionView!.bounds)
+        }
+    }
+    
+    override func collectionViewContentSize() -> CGSize {
+        return CGSize(width: width, height: contentHeight)
+    }
+    
+    override func prepareLayout() {
+        if cache.isEmpty {
+            let columnWidth = width / CGFloat(numberOfColumns)
+            
+            var xOffsets = [CGFloat]()
+            for column in 0..<numberOfColumns {
+                xOffsets.append(CGFloat(column) * columnWidth)
+            }
+            
+            var yOffsets = [CGFloat](count: numberOfColumns, repeatedValue: 0)
+            
+            var column = 0
+            for item in 0..<collectionView!.numberOfItemsInSection(0) {
+                let indexPath = NSIndexPath(forItem: item, inSection: 0)
+                let height = delegate.collectionView(collectionView!, heightForItemAtIndexPath: indexPath)
+                let frame = CGRect(x: xOffsets[column], y: yOffsets[column], width: columnWidth, height: height)
+                let attributes = UICollectionViewLayoutAttributes(forCellWithIndexPath: indexPath)
+                attributes.frame = frame
+                cache.append(attributes)
+                contentHeight = max(contentHeight, CGRectGetMaxY(frame))
+                yOffsets[column] = yOffsets[column] + height
+                column = column >= (numberOfColumns - 1) ? 0 : ++column
+            }
+        }
+    }
+    
+    override func layoutAttributesForElementsInRect(rect: CGRect) -> [UICollectionViewLayoutAttributes]? {
+        var layoutAttributes = [UICollectionViewLayoutAttributes]()
+        for attributes in cache {
+            if CGRectIntersectsRect(attributes.frame, rect) {
+                layoutAttributes.append(attributes)
+            }
+        }
+        return layoutAttributes
+    }
+    
+}
--- hackathon-for-hunger/DashboardViewController.swift
+++ hackathon-for-hunger/DashboardViewController.swift
@@ -29,24 +29,11 @@ class DashboardViewController: UIViewController {
     @IBOutlet weak var collectionView: UICollectionView!
     
     //the width of collectionViewCell
-    var width: CGFloat!
-    
-    
-    
-    override func viewDidLayoutSubviews() {
-        super.viewDidLayoutSubviews()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
         
-        // Lay out the collection view so that cells take up 1 of the width,
         
-        let layout : UICollectionViewFlowLayout = UICollectionViewFlowLayout()
-        layout.sectionInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
-        layout.minimumLineSpacing = 0
-        layout.minimumInteritemSpacing = 0
-        
-        width = floor(collectionView.frame.size.width)
-        layout.itemSize = CGSize(width: width, height: width)
-        
-        collectionView.collectionViewLayout = layout
     }
     
     
@@ -54,28 +41,50 @@ class DashboardViewController: UIViewController {
 }
 
 
-extension DashboardViewController: UICollectionViewDelegate, UICollectionViewDataSource{
-    
+//extension DashboardViewController: UICollectionViewDelegate, UICollectionViewDataSource{
+//    
+//
+//    
+//    
+//    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+//        return data.count
+//    }
+//    
+//    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+//        
+//        let reuseIdentifier = "informationCell"
+//        
+//        let cell = collectionView.dequeueReusableCellWithReuseIdentifier(reuseIdentifier, forIndexPath: indexPath) as! DashboardCollectionViewCell
+//        
+//        let information = data[indexPath.row]
+//        cell.configure(information)
+//        return cell
+//        
+//    }
+//    
+//    
+//    
+//    
+//}
 
+extension DashboardViewController: DashboardLayoutDelegate {
     
-    
-    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return data.count
+    func collectionView(collectionView: UICollectionView, heightForItemAtIndexPath indexPath: NSIndexPath) -> CGFloat{
+        return 100
     }
-    
-    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
-        
-        let reuseIdentifier = "informationCell"
-        
-        let cell = collectionView.dequeueReusableCellWithReuseIdentifier(reuseIdentifier, forIndexPath: indexPath) as! DashboardCollectionViewCell
-        
-        let information = data[indexPath.row]
-        cell.configure(information)
-        return cell
-        
-    }
-    
-    
-    
     
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
