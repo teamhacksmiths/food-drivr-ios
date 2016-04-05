ec1b99391ab28c3d5cd78181861c0440363e3310
Ian Gristock <iangristock@gmail.com>
2016-04-05 18:55:48 +0100

    feat: added intital main navigaiton

--- Pods/Target Support Files/Pods-hackathon-for-hunger/Pods-hackathon-for-hunger-frameworks.sh
+++ Pods/Target Support Files/Pods-hackathon-for-hunger/Pods-hackathon-for-hunger-frameworks.sh
@@ -87,9 +87,11 @@ if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "Pods-hackathon-for-hunger/Alamofire.framework"
   install_framework "Pods-hackathon-for-hunger/Realm.framework"
   install_framework "Pods-hackathon-for-hunger/RealmSwift.framework"
+  install_framework "Pods-hackathon-for-hunger/SlideMenuControllerSwift.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "Pods-hackathon-for-hunger/Alamofire.framework"
   install_framework "Pods-hackathon-for-hunger/Realm.framework"
   install_framework "Pods-hackathon-for-hunger/RealmSwift.framework"
+  install_framework "Pods-hackathon-for-hunger/SlideMenuControllerSwift.framework"
 fi
