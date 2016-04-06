c37f6cca433718127e55f2162fca75402698c64b
Anas Belkhadir <belkhadir.anas@gmail.com>
2016-04-07 00:11:04 +0100

    chore: remove AppId an DisplayName

--- hackathon-for-hunger/AppDelegate.swift
+++ hackathon-for-hunger/AppDelegate.swift
@@ -18,8 +18,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
         
-        FBSDKSettings.setAppID("256802277988898")
-        FBSDKSettings.setDisplayName("hacksmith")
+        FBSDKSettings.setAppID("")
+        FBSDKSettings.setDisplayName("")
         return FBSDKApplicationDelegate.sharedInstance().application(application, didFinishLaunchingWithOptions: launchOptions)
         
     }
