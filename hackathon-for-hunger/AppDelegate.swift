bcbdd629d7be7db9485014deb1d05ad6b6355293
Anas Belkhadir <belkhadir.anas@gmail.com>
2016-04-06 16:20:46 +0100

    feat: adding the appId and the name

--- hackathon-for-hunger/AppDelegate.swift
+++ hackathon-for-hunger/AppDelegate.swift
@@ -17,9 +17,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
-
+        
+        FBSDKSettings.setAppID("256802277988898")
+        FBSDKSettings.setDisplayName("hacksmith")
         return FBSDKApplicationDelegate.sharedInstance().application(application, didFinishLaunchingWithOptions: launchOptions)
-
+        
     }
 
     
