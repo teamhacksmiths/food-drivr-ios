f6ee5decc10771ec6667766380f5035f7ff10c71
Anas Belkhadir <belkhadir.anas@gmail.com>
2016-04-11 19:25:34 +0100

    chore: adding Pending Donation

---
+++ hackathon-for-hunger/PendingDonationsDashboard.swift
@@ -0,0 +1,86 @@
+//
+//  PendingDonationsDashboard.swift
+//  hackathon-for-hunger
+//
+//  Created by Anas Belkhadir on 11/04/2016.
+//  Copyright © 2016 Hacksmiths. All rights reserved.
+//
+
+import UIKit
+
+class PendingDonationsDashboard: UITableViewController {
+    
+    
+    var mockData = [("baba","baba","baba"),
+                    ("prapra","prapra","prapra")]
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.separatorColor = UIColor.blueColor()
+    }
+    
+    //Mark - Create the HeaderView to our tableView
+    lazy var containerView: UIView = {
+        
+        let placeHolderImage = UIImageView()
+        let fullName = UILabel()
+        fullName.text = "anas belkhadir"
+        
+        
+        let containerStackView = UIStackView(arrangedSubviews: [placeHolderImage, fullName])
+        containerStackView.axis = .Vertical
+        containerStackView.spacing = 10.0
+        containerStackView.alignment = .Center
+        containerStackView.distribution = .EqualCentering
+        
+        
+        
+        return containerStackView
+    }()
+    
+    override func tableView(tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        return containerView
+    }
+    
+    override func tableView(tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 140.0
+    }
+    
+    //Mark - Setting up tableView
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return mockData.count
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let identifier = "pendingDonation"
+        let cell = tableView.dequeueReusableCellWithIdentifier(identifier, forIndexPath: indexPath) as! PendingDonationsDashboardTableViewCell
+        cell.delegate = self
+        cell.indexPath = indexPath
+        cell.information = mockData[indexPath.row]
+        
+        return cell
+    }
+}
+
+
+
+extension PendingDonationsDashboard: PendingDonationsDashboardTableViewCellDelegate {
+    
+    func cell(cell: PendingDonationsDashboardTableViewCell, didPress indexPath: NSIndexPath) {
+        mockData.removeAtIndex(indexPath.row)
+        tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Automatic)
+    }
+
+}
+
+
+
+
+
+
+
+
+
