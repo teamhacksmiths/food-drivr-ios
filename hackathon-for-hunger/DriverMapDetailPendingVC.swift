b82f58ce1a8bd9823052b702fb0842ac6e577365
David Fierstein <studio@davidiad.com>
2016-04-17 22:28:50 -0700

    feat: Zoom detail map such that pins are in view

--- hackathon-for-hunger/DriverMapDetailPendingVC.swift
+++ hackathon-for-hunger/DriverMapDetailPendingVC.swift
@@ -14,7 +14,7 @@ class DriverMapDetailPendingVC: UIViewController, MKMapViewDelegate {
     
     var startingRegion = MapsDummyData.sharedInstance.startingRegion // used to retrieve precalculated starting region
     
-    var annotationsHaveBeenShown: Bool = false //to allow auto zooming to pins, but just once
+    //var annotationsHaveBeenShown: Bool = false //to allow auto zooming to pins, but just once
     
     var locationManager = LocationManager.sharedInstance.locationManager
     
@@ -48,7 +48,7 @@ class DriverMapDetailPendingVC: UIViewController, MKMapViewDelegate {
     override func viewDidLoad() {
 
         mapView.delegate = self
-        mapView.setRegion(startingRegion, animated: true)
+        //mapView.setRegion(startingRegion, animated: true)
         if donation != nil {
             donorNameLabel.text = donation?.donor?.name
             // TODO: need a street address for Donor Participant, to be passed to UI
@@ -81,15 +81,59 @@ class DriverMapDetailPendingVC: UIViewController, MKMapViewDelegate {
         
     }
     
+    func zoomToFitMapAnnotations() {
+        if mapView.annotations.count == 0 {
+            mapView.setRegion(startingRegion, animated: true)
+            return
+        }
+        
+        var topLeftCoord = CLLocationCoordinate2D(latitude: -90, longitude: 180)
+        var bottomRightCoord = CLLocationCoordinate2D(latitude: 90, longitude: -180)
+        
+        for annotation in mapView.annotations {
+            topLeftCoord.longitude = min(topLeftCoord.longitude, annotation.coordinate.longitude)
+            topLeftCoord.latitude = max(topLeftCoord.latitude, annotation.coordinate.latitude)
+            bottomRightCoord.longitude = max(bottomRightCoord.longitude, annotation.coordinate.longitude)
+            bottomRightCoord.latitude = min(bottomRightCoord.latitude, annotation.coordinate.latitude)
+        }
+        
+        var region = MKCoordinateRegion()
+        region.center.latitude = topLeftCoord.latitude - (topLeftCoord.latitude - bottomRightCoord.latitude) * 0.5
+        region.center.longitude = topLeftCoord.longitude + (bottomRightCoord.longitude - topLeftCoord.longitude) * 0.5
+        
+        // Add a little extra space on the sides
+        region.span.latitudeDelta = abs(topLeftCoord.latitude - bottomRightCoord.latitude) * 1.3
+        region.span.longitudeDelta = abs(bottomRightCoord.longitude - topLeftCoord.longitude) * 1.3
+        
+        region = mapView.regionThatFits(region)
+        //mapView.setRegion(region, animated: true)
+        mapView.setVisibleMapRect(MKMapRectForCoordinateRegion(region), edgePadding: UIEdgeInsets(top: 50, left: 50, bottom: 0, right: 50), animated: true)
+    }
+    
+    func MKMapRectForCoordinateRegion(region:MKCoordinateRegion) -> MKMapRect {
+        let topLeft = CLLocationCoordinate2D(latitude: region.center.latitude + (region.span.latitudeDelta/2), longitude: region.center.longitude - (region.span.longitudeDelta/2))
+        let bottomRight = CLLocationCoordinate2D(latitude: region.center.latitude - (region.span.latitudeDelta/2), longitude: region.center.longitude + (region.span.longitudeDelta/2))
+        
+        let a = MKMapPointForCoordinate(topLeft)
+        let b = MKMapPointForCoordinate(bottomRight)
+        
+        return MKMapRect(origin: MKMapPoint(x:min(a.x,b.x), y:min(a.y,b.y)), size: MKMapSize(width: abs(a.x-b.x), height: abs(a.y-b.y)))
+    }
+    
+    
     //MARK:- mapView
     
     func mapView(mapView: MKMapView, didUpdateUserLocation userLocation: MKUserLocation) {
         
-        if annotationsHaveBeenShown != true {
-            annotationsHaveBeenShown = true
-
-            mapView.showAnnotations(mapView.annotations, animated: true)
-        }
+//        if annotationsHaveBeenShown != true {
+//            annotationsHaveBeenShown = true
+        
+        // mapView.showAnnotations(mapView.annotations, animated: true)
+        zoomToFitMapAnnotations()
+//            
+//            mapView.setVisibleMapRect(mapView.visibleMapRect, edgePadding: UIEdgeInsets(top: 10, left: 10, bottom: 0, right: 10), animated: true)
+    //}
+        //zoomToFitMapAnnotations()
         
     }
     
@@ -168,12 +212,15 @@ class DriverMapDetailPendingVC: UIViewController, MKMapViewDelegate {
             
         }
         
+
+        for annotation in mapView.annotations {
+            mapView.selectAnnotation(annotation, animated: true)
+        }
+        
+        zoomToFitMapAnnotations() // calling too early here - no user location yet?
+        
     }
     
-//    // keep all callouts visible
-//    func mapView(mapView: MKMapView, didDeselectAnnotationView view: MKAnnotationView) {
-//        mapView.selectAnnotation(view.annotation!, animated: false)
-//    }
     
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         if (segue.identifier == "toDriverPickupMap") {
