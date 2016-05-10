1c92cc944a2b32bdbdfd5a349f8a4408a51f137d
David Fierstein <studio@davidiad.com>
2016-05-09 17:09:10 -0700

    feat: Add user GET request to user profile view

--- hackathon-for-hunger/Modules/User/Views/ProfileViewController.swift
+++ hackathon-for-hunger/Modules/User/Views/ProfileViewController.swift
@@ -10,10 +10,67 @@ import UIKit
 
 class ProfileViewController: UIViewController {
     
+    @IBOutlet weak var userName: UILabel!
+    @IBOutlet weak var email: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.setupMenuBar()
         self.title = "Profile"
+        
+        let session = NSURLSession.sharedSession()
+        
+        let url = NSURL(string: "https://wastenotfoodtaxi.herokuapp.com/api/v1/users/ePQ1M_71enVov2zzdQrg")!
+        let request = NSURLRequest(URL: url)
+        
+        // Initialize task for getting data
+        let task = session.dataTaskWithRequest(request) { (data, response, error) in
+            
+            // Check for a successful response
+            // GUARD: Was there an error?
+            guard (error == nil) else {
+                print("There was an error with your request: \(error)")
+                return
+            }
+            
+            // GUARD: Did we get a successful 2XX response?
+            guard let statusCode = (response as? NSHTTPURLResponse)?.statusCode where statusCode >= 200 && statusCode <= 299 else {
+                if let response = response as? NSHTTPURLResponse {
+                    print("Your request returned an invalid response! Status code: \(response.statusCode)!")
+                } else if let response = response {
+                    print("Your request returned an invalid response! Response: \(response)!")
+                } else {
+                    print("Your request returned an invalid response!")
+                }
+                return
+            }
+            
+            // GUARD: Was there any data returned?
+            guard let data = data else {
+                print("No data was returned by the request!")
+                return
+            }
+            
+            // - Parse the data
+            let parsedResult: AnyObject!
+            do {
+                parsedResult = try NSJSONSerialization.JSONObjectWithData(data, options: .AllowFragments)
+            } catch {
+                parsedResult = nil
+                print("Could not parse the data as JSON: '\(data)'")
+                return
+            }
+            
+            print("PARSED RESULT: \(parsedResult)")
+            
+//            // GUARD: Did Flickr return an error (stat != ok)?
+//            guard let stat = parsedResult["stat"] as? String where stat == "ok" else {
+//                print("Flickr API returned an error. See error code and message in \(parsedResult)")
+//                return
+//            }
+        }
+        
+        task.resume()
     }
     
     @IBAction func toggleMenu(sender: AnyObject) {
