ea41c2ab5d2e92c0cb2db706eaab1aeedcb9bc56
David Fierstein <studio@davidiad.com>
2016-05-14 09:05:46 -0700

    chore: Change TF to TextField in var names

--- hackathon-for-hunger/Modules/User/Views/ProfileViewController.swift
+++ hackathon-for-hunger/Modules/User/Views/ProfileViewController.swift
@@ -4,18 +4,23 @@
 //
 //  Created by Anas Belkhadir on 15/04/2016.
 //  Copyright © 2016 Hacksmiths. All rights reserved.
-//
+//  Edits by David Fierstein 11/05/2016
 
 import UIKit
 
-class ProfileViewController: UIViewController {
+class ProfileViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UIGestureRecognizerDelegate {
     
 
     private let userService: UserService = UserService()
 
  
-    @IBOutlet weak var userNameTF: UITextField!
-    @IBOutlet weak var emailTF: UITextField!
+    @IBOutlet weak var userNameTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    @IBOutlet weak var avatarView: UIView!
+    @IBOutlet weak var avatarImageView: UIImageView!
+    
+    
     
     @IBAction func save(sender: AnyObject) {
         print("Save pressed")
@@ -27,21 +32,53 @@ class ProfileViewController: UIViewController {
         self.setupMenuBar()
         self.title = "Profile"
         let currentUser = AuthService.sharedInstance.getCurrentUser()
-        userNameTF.text = currentUser?.name
-        emailTF.text = currentUser?.email
+        userNameTextField.text = currentUser?.name
+        emailTextField.text = currentUser?.email
 
+        let tapGestureRecognizer = UITapGestureRecognizer(target:self, action:#selector(ProfileViewController.avatarTapped(_:)))
+        avatarView.addGestureRecognizer(tapGestureRecognizer)
+        avatarView.invalidateIntrinsicContentSize()
+        
+        avatarView.contentMode = .ScaleAspectFill
+        avatarImageView.contentMode = .ScaleAspectFill
+    }
+    
+    func avatarTapped(img: AnyObject) {
+        pickAnImageFromAlbum(img)
+    }
+    
+    func pickAnImageFromAlbum(sender: AnyObject) {
+        let pickerController = UIImagePickerController()
+        pickerController.delegate = self
+        pickerController.sourceType = UIImagePickerControllerSourceType.PhotoLibrary
+        presentViewController(pickerController, animated: true, completion: nil)
+    }
+    
+    //MARK:- Image Picker functions
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        self.dismissViewControllerAnimated(true, completion: nil)
+        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            avatarImageView.image = image
 
+        }
+        
+    }
+    
+    func imagePickerControllerDidCancel(picker: UIImagePickerController) {
+        //checkPickingInLandscape()
+        self.dismissViewControllerAnimated(true, completion: nil)
     }
     
     func updateUser() {
         
         
         var user = UserRegistration()
-        user.email = emailTF.text ?? "donor@hacksmiths.com"
-        user.phone = "3211231234"
-        user.name = userNameTF.text ?? "please enter your name"
+        user.email = emailTextField.text ?? "donor@hacksmiths.com"
+        //user.phone = "3211231234"
+        user.name = userNameTextField.text ?? "please enter your name"
         user.password = "password"
-        user.role = .Donor
+        //user.role = .Donor
         
         
         
@@ -58,5 +95,29 @@ class ProfileViewController: UIViewController {
     
     @IBAction func toggleMenu(sender: AnyObject) {
         self.slideMenuController()?.openLeft()
+    }
+    
+    //MARK: - Text and keyboard function
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    //MARK:- Gesture Recognizer functions
+    func gestureRecognizer(gestureRecognizer: UIGestureRecognizer, shouldReceiveTouch touch: UITouch) -> Bool {
+        // Don't let tap GR hide textfields if a textfield is being touched for editing
+        if touch.view == userNameTextField || touch.view == emailTextField {
+            return false
+        }
+        // Anywhere else on the screen, allow the tap gesture recognizer to hideToolBars
+        return true
+    }
+    
+    // Cancels textfield editing when user touches outside the textfield
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        if userNameTextField.isFirstResponder() || emailTextField.isFirstResponder() {
+            view.endEditing(true)
+        }
+        super.touchesBegan(touches, withEvent:event)
     }
 }
---
+++ hackathon-for-hunger/UserData.swift
@@ -0,0 +1,41 @@
+//
+//  UserData.swift
+//  hackathon-for-hunger
+//
+//  Created by David Fierstein on 5/14/16.
+//  Copyright © 2016 Hacksmiths. All rights reserved.
+//  data model for updating user profile
+//  
+//
+//  based on RegistrationDTO.swift by Ian Gristock
+//  hackathon-for-hunger
+
+
+import Foundation
+import JSONCodable
+
+struct UserData {
+    
+    
+    var name: String?
+    var phone: String?
+    var email: String?
+    var password: String?
+    var password_confirmation: String?
+    var avatar: String?
+    
+}
+
+extension UserData: JSONEncodable {
+    func toJSON() throws -> [String:AnyObject] {
+        return try JSONEncoder.create({ (encoder) -> Void in
+            try encoder.encode(name, key: "name")
+            try encoder.encode(phone, key: "phone")
+            try encoder.encode(email, key: "email")
+            try encoder.encode(password, key: "password")
+            try encoder.encode(password_confirmation, key: "password_confirmation")
+            try encoder.encode(avatar, key: "avatar")
+            
+        })
+    }
+}
