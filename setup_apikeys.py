b7c1aa41c087ad9a316e12cd4ac05e7a778723d2
Ryan Collins <rccproductions@mac.com>
2016-08-23 18:26:09 -0400

    Fix: setup the script to properly copy api keys file

--- setup_apikeys.py
+++ setup_apikeys.py
@@ -1,6 +1,24 @@
-import subprocess
+from subprocess import call
+import os
+
+directory = './hackathon-for-hunger'
+filename = 'ApiKeysDist.plist'
+new_filename = 'ApiKeys.plist'
+
+def clearOldFile(dir):
+    old_files = os.listdir(dir)
+    for file_name in old_files:
+        full_file_name = os.path.join(dir, file_name)
+        if (os.path.isfile(full_file_name)):
+            if "ApiKeys.plist" in file_name:
+                os.remove(full_file_name)
+
 
 def copyPlist():
-    subprocess.call(['cp', './hackathon-for-hunger/ApiKeysDist.plist', './hackathon-for-hunger/ApiKeys4.plist'])
+    clearOldFile(directory)
+    file_one = os.path.join(directory, filename)
+    file_two = os.path.join(directory, new_filename)
+    call(['cp', file_one, file_two])
+    print("The file ApiKeysDist.plist has been copied to ApiKeys.plist")
 
 copyPlist()
